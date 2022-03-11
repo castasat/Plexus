193980045b567606e5bef5d8f395ec6005385d70
DenisDmitrienko <DVDmitrienko@sberbank.ru>
2022-03-12 02:17:48 +0300

    switched to nearby connection api, tested advertising, discovering, finding endpoint, accepting connection, getting  connection result

--- app/src/main/java/p/l/e/x/u/s/PlexusActivity.kt
+++ app/src/main/java/p/l/e/x/u/s/PlexusActivity.kt
@@ -1,14 +1,80 @@
 package p.l.e.x.u.s
 
+import android.Manifest.permission.*
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build.*
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import p.l.e.x.u.s.PlexusApp.Companion.log
+import p.l.e.x.u.s.PlexusViewModel.Companion.CODE
 
 class PlexusActivity : AppCompatActivity() {
+    private var allPermissionsGranted: Boolean = true
     private val viewModel: PlexusViewModel by viewModels()
+
+    private val advertiseButton: Button by lazy { findViewById(R.id.advertiseButton) }
+    private val discoverButton: Button by lazy { findViewById(R.id.discoverButton) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout)
+        observeLiveData()
         viewModel.init()
+        advertiseButton.setOnClickListener { viewModel.advertise() }
+        discoverButton.setOnClickListener { viewModel.discover() }
+    }
+
+    private fun observeLiveData() {
+        observeRequestRuntimePermissionLiveData()
+    }
+
+    private fun observeRequestRuntimePermissionLiveData() {
+        viewModel.requestRuntimePermissionLiveData
+            .observe(this) { permissionsToRequestAtRuntime: Array<String> ->
+                if (VERSION.SDK_INT >= VERSION_CODES.M) {
+                    for (permission in permissionsToRequestAtRuntime) {
+                        if (checkSelfPermission(permission) != PERMISSION_GRANTED) {
+                            requestPermissions(permissionsToRequestAtRuntime, CODE)
+                        }
+                    }
+                } else {
+                    log(
+                        "PlexusActivity.observeRequestRuntimePermissionLiveData(): " +
+                                "permission granted or API < 23 (M, Android 6)"
+                    )
+                }
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CODE) {
+            // находим индекс в массиве permissions и проверяем значение
+            // из массива grantResults с этим индексом
+            for (i in permissions.indices) {
+                log("PlexusActivity.onRequestPermissionsResult(): permission = ${permissions[i]}")
+                when (permissions[i]) {
+                    ACCESS_FINE_LOCATION,
+                    BLUETOOTH_ADVERTISE,
+                    BLUETOOTH_CONNECT,
+                    BLUETOOTH_SCAN,
+                    READ_EXTERNAL_STORAGE -> allPermissionsGranted =
+                        if (grantResults[i] == PERMISSION_GRANTED) {
+                            log("PlexusActivity.onRequestPermissionsResult(): permission granted")
+                            allPermissionsGranted
+                        } else {
+                            log("PlexusActivity.onRequestPermissionsResult(): permission denied")
+                            false
+                        }
+                    else -> log("PlexusActivity.onRequestPermissionsResult(): unknown permission")
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
--- app/src/main/java/p/l/e/x/u/s/PlexusViewModel.kt
+++ app/src/main/java/p/l/e/x/u/s/PlexusViewModel.kt
@@ -1,39 +1,151 @@
 package p.l.e.x.u.s
 
+import android.Manifest.permission.*
+import android.annotation.SuppressLint
 import android.app.Application
-import android.content.IntentFilter
-import android.net.wifi.p2p.WifiP2pManager
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build.*
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes.*
+import com.google.android.gms.nearby.connection.Strategy.P2P_CLUSTER
+import p.l.e.x.u.s.PlexusApp.Companion.log
 
 class PlexusViewModel(application: Application) : AndroidViewModel(application) {
-    private val broadcastReceiver = PlexusReceiver()
-    private val plexusManager: WifiP2pManager? by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        getApplication<Application>().applicationContext
-            .getSystemService(AppCompatActivity.WIFI_P2P_SERVICE) as WifiP2pManager?
+    @SuppressLint("StaticFieldLeak")
+    private val appContext = getApplication<Application>().applicationContext
+    private val _requestRuntimePermissionLiveData = MutableLiveData<Array<String>>()
+    val requestRuntimePermissionLiveData: LiveData<Array<String>>
+        get() = _requestRuntimePermissionLiveData
+
+    private val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            log("PlexusViewModel.onPayloadReceived(): $endpointId payload = $payload")
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            log("PlexusViewModel.onPayloadTransferUpdate(): $endpointId update = $update")
+        }
+
     }
 
+    // for advertise
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, info: ConnectionInfo) {
+            log("PlexusViewModel.onConnectionInitiated(): $endpointId connectionInfo = $info")
+            Nearby
+                .getConnectionsClient(appContext)
+                // TODO give opportunity to reject connection
+                .acceptConnection(endpointId, payloadCallback)
+        }
 
-    fun init() {
-        with(getApplication<Application>().applicationContext) {
-            // TODO
-            val channel = plexusManager?.initialize(this, mainLooper, null)
-            registerReceiver(
-                broadcastReceiver,
-                IntentFilter().apply {
-                    addAction(WifiP2pManager.WIFI_P2P_DISCOVERY_CHANGED_ACTION)
-                    addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
-                    addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
-                    addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
-                    addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
-                }
-            )
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            log("PlexusViewModel.onConnectionResult(): $endpointId connectionResolution = $result")
+            when (result.status.statusCode) {
+                STATUS_OK ->
+                    log("PlexusViewModel.onConnectionResult(): STATUS_OK")
+                STATUS_CONNECTION_REJECTED ->
+                    log("PlexusViewModel.onConnectionResult(): STATUS_CONNECTION_REJECTED")
+                STATUS_ERROR ->
+                    log("PlexusViewModel.onConnectionResult(): STATUS_ERROR")
+                else -> log(
+                    "PlexusViewModel.onConnectionResult(): " +
+                            "statusCode = ${result.status.statusCode}"
+                )
+            }
+        }
 
+        override fun onDisconnected(endpointId: String) {
+            log("PlexusViewModel.onDisconnected() p0")
         }
     }
 
-    override fun onCleared() {
-        getApplication<Application>().applicationContext.unregisterReceiver(broadcastReceiver)
-        super.onCleared()
+    // for discover
+    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+            log("PlexusViewModel.onEndpointFound(): $endpointId discoveryEndpointInfo = $info")
+            Nearby
+                .getConnectionsClient(appContext)
+                .requestConnection(ADVERTISING, endpointId, connectionLifecycleCallback)
+                .addOnSuccessListener { log("PlexusViewModel.onEndpointFound(): success") }
+                .addOnFailureListener { exception ->
+                    log("PlexusViewModel.onEndpointFound(): error = $exception")
+                    exception.printStackTrace()
+                }
+        }
+
+        override fun onEndpointLost(endpointId: String) {
+            log("PlexusViewModel.onEndpointLost(): $endpointId")
+        }
+
+    }
+
+    fun init() {
+        val permissions =
+            if (VERSION.SDK_INT >= VERSION_CODES.S) {
+                arrayOf(
+                    READ_EXTERNAL_STORAGE,
+                    ACCESS_FINE_LOCATION,
+                    BLUETOOTH_ADVERTISE,
+                    BLUETOOTH_CONNECT,
+                    BLUETOOTH_SCAN
+                )
+            } else if (VERSION.SDK_INT >= VERSION_CODES.M) {
+                arrayOf(
+                    READ_EXTERNAL_STORAGE,
+                    ACCESS_FINE_LOCATION
+                )
+            } else {
+                emptyArray()
+            }
+        requestRuntimePermissionCheck(permissions)
+    }
+
+    private fun requestRuntimePermissionCheck(permissions: Array<String>) =
+        _requestRuntimePermissionLiveData.postValue(permissions)
+
+    fun advertise() {
+        log("PlexusViewModel.advertise()")
+        // TODO start advertising only once
+        // TODO stop advertising
+        Nearby
+            .getConnectionsClient(appContext)
+            .startAdvertising(
+                ADVERTISING,
+                SERVICE_ID,
+                connectionLifecycleCallback,
+                AdvertisingOptions.Builder().setStrategy(P2P_CLUSTER).build()
+            )
+            .addOnSuccessListener { log("PlexusViewModel.advertise(): success") }
+            .addOnFailureListener { exception ->
+                log("PlexusViewModel.advertise(): error = $exception")
+                exception.printStackTrace()
+            }
+    }
+
+    fun discover() {
+        log("PlexusViewModel.discover()")
+        // TODO start discovering only once
+        // TODO stop discovering
+        Nearby
+            .getConnectionsClient(appContext)
+            .startDiscovery(
+                SERVICE_ID,
+                endpointDiscoveryCallback,
+                DiscoveryOptions.Builder().setStrategy(P2P_CLUSTER).build()
+            )
+            .addOnSuccessListener { log("PlexusViewModel.discover(): success") }
+            .addOnFailureListener { exception ->
+                log("PlexusViewModel.discover(): error = $exception")
+                exception.printStackTrace()
+            }
+    }
+
+    companion object {
+        const val CODE: Int = 0xC0D3
+        private const val SERVICE_ID = "p.l.e.x.u.s"
+        private const val ADVERTISING = "advertising"
     }
 }
